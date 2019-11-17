36185c14865277886065afb56a78848130c3bde1
Robert Holmberg <holmbrob@cs.helsinki.fi>
2019-11-17 18:57:00 +0200

    added selenium testing for task 8

--- viikko3/tehtava7/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtava7/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,20 +15,80 @@ public class Tester {
         
         sleep(2);
         
-        WebElement element = driver.findElement(By.linkText("login"));
+//      Logging in with correct username and password
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        sleep(2);
+
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//
+//        sleep(2);
+//        element.submit();
+
+//      Logging in with correct username and wrong password
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        sleep(2);
+//        
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("wrong");
+//        element = driver.findElement(By.name("login"));
+//        
+//        sleep(2);
+//        element.submit();
+
+//      Creating a new user
+//        WebElement element = driver.findElement(By.linkText("register new user"));
+//        element.click();
+//        
+//        sleep(2);
+//        
+//        Random rand = new Random();
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("testi" + rand.nextInt(10000));
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("passw0rd");
+//        element = driver.findElement(By.name("passwordConfirmation"));
+//        element.sendKeys("passw0rd");
+//        element = driver.findElement(By.name("signup"));
+//        
+//        sleep(2);
+//        element.submit();
+        
+//      Creating a new user and logging out
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
-
+        
         sleep(2);
-
+        
+        Random rand = new Random();
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("testi" + rand.nextInt(10000));
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("passw0rd");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("passw0rd");
+        element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
 
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        
         sleep(3);
         
         driver.quit();
